390102a8c9b2c87f6422d4e189006b4b7f054223
Nick <nickdekok.nl@gmail.com>
2023-11-22 12:56:21 +0100

    initial commit, clone audiorecorder project

--- app/src/main/java/com/plcoding/audiorecorder/MainActivity.kt
+++ app/src/main/java/com/plcoding/audiorecorder/MainActivity.kt
@@ -8,13 +8,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import com.plcoding.audiorecorder.playback.AndroidAudioPlayer
 import com.plcoding.audiorecorder.record.AndroidAudioRecorder
