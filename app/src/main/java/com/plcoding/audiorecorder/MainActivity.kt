74f02b0e0c5414961bc9612ae83dac5b8fcf904a
Nick de Kok <nickdekok.nl@gmail.com>
2024-01-01 13:15:00 +0100

    Revert "View/button"

--- app/src/main/java/com/plcoding/audiorecorder/MainActivity.kt
+++ app/src/main/java/com/plcoding/audiorecorder/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.plcoding.audiorecorder
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -14,7 +12,6 @@ import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import com.plcoding.audiorecorder.playback.AndroidAudioPlayer
 import com.plcoding.audiorecorder.record.AndroidAudioRecorder
 import com.plcoding.audiorecorder.ui.theme.AudioRecorderTheme
@@ -32,7 +29,6 @@ class MainActivity : ComponentActivity() {
 
     private var audioFile: File? = null
 
-    @SuppressLint("SetTextI18n", "MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ActivityCompat.requestPermissions(
@@ -40,32 +36,37 @@ class MainActivity : ComponentActivity() {
             arrayOf(Manifest.permission.RECORD_AUDIO),
             0
         )
-
-        setContentView(R.layout.activity_main)
-        val listenButton: Button = findViewById(R.id.listenButton)
-        listenButton.setOnClickListener {
-            if (listenButton.text == "Start Listening") {
-                listenButton.text = "Stop Listening"
-                listenButton.setBackgroundColor(ContextCompat.getColor(this, R.color.buttercream))
-                File(cacheDir, "audio.mp3").also {
-                    recorder.start(it)
-                    audioFile = it
+        setContent {
+            AudioRecorderTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = {
+                        File(cacheDir, "audio.mp3").also {
+                            recorder.start(it)
+                            audioFile = it
+                        }
+                    }) {
+                        Text(text = "Start recording")
+                    }
+                    Button(onClick = {
+                        recorder.stop()
+                    }) {
+                        Text(text = "Stop recording")
+                    }
+                    Button(onClick = {
+                        player.playFile(audioFile ?: return@Button)
+                    }) {
+                        Text(text = "Play")
+                    }
+                    Button(onClick = {
+                        player.stop()
+                    }) {
+                        Text(text = "Stop playing")
+                    }
                 }
-            } else {
-                listenButton.text = "Start Listening"
-                listenButton.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
-                recorder.stop()
-            }
-        }
-
-        val playButton: Button = findViewById(R.id.playButton)
-        playButton.setOnClickListener{
-            if (playButton.text == "Play") {
-                playButton.text = "Stop"
-                player.playFile(audioFile ?: return@setOnClickListener)
-            } else {
-                playButton.text = "Play"
-                player.stop()
             }
         }
     }
